ab20326dcbfe67de9ba3cdca6949a6cb110a07bb
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-06-09 08:34:58 +0200

    React 2/4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+import { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+  },
+])
+
+//Delete Task 
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
 }
-//_______________Classes________
-// class App extends React.Component{
-//   render(){
-//     return<h1>hello</h1>
-//   }
-// }
+
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+      task.id == id ? { ...task, reminder:
+      !task.reminder} : task
+    )
+  )
+}
+
+  return (
+    <div className='container'>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        'No Tasks To Show'
+        )}
+    </div>
+  )
+}
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
-
+const Button = ({ color, text, onClick }) => {
   return <button onClick={onClick} style={{ backgroundColor: color}} className='btn'>{text}</button>
 }
 
@@ -16,6 +11,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
